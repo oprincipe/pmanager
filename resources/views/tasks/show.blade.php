9a532ca0111eb0cfd212be3fafef600a14034cea
Orazio Principe <orazio.principe@gmail.com>
2017-12-09 17:05:35 +0100

    Add role_id to company list

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -89,39 +89,42 @@
     </div>
 
     <div class="pull-right col-xs-12 col-sm-3 col-md-3 col-lg-3 blog-sidebar">
-        <div class="sidebar-module">
-            <h4>Actions</h4>
-            <ol class="list-unstyled">
-                <li><a href="{{ route('tasks.edit', $task->id) }}"><i class="fa fa-edit"></i> Edit task</a></li>
-                <li><a href="{{ URL::to('/companies/'.$task->company_id) }}"><i class="fa fa-building"></i> View company</a></li>
-                <li><a href="{{ URL::to('/projects/'.$task->project_id) }}"><i class="fa fa-briefcase"></i> View project</a></li>
-                <li><a href="{{ URL::to('/companies/') }}"><i class="fa fa-list"></i> All companies</a></li>
-
-                @if (Auth::user()->role_id == 1)
-                <br />
-                <a href="#"
-                   onclick="
-                           var result = confirm('Are you sure you wish to delete this task?');
-                           if(result) {
-                           event.preventDefault();
-                           $('#delete-task-{{ $task->id }}').submit();
-                           }"><i class="fa fa-trash"></i> Delete task</a>
-
-                <form id="delete-task-{{ $task->id }}" action="{{ route("tasks.destroy", [$task->id]) }}"
-                      method="post" style="display: none">
-                    {{ csrf_field() }}
-                    <input type="hidden" name="_method" value="delete" />
-                </form>
-                @endif
-
-            </ol>
-
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    @include("partials.comments")
-                </div>
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <h3 class="panel-title">Actions</h3>
             </div>
+            <div class="panel-body">
+                <ol class="list-unstyled">
+                    <li><a href="{{ route('tasks.edit', $task->id) }}"><i class="fa fa-edit"></i> Edit task</a></li>
+                    <li><a href="{{ URL::to('/companies/'.$task->company_id) }}"><i class="fa fa-building"></i> View company</a></li>
+                    <li><a href="{{ URL::to('/projects/'.$task->project_id) }}"><i class="fa fa-briefcase"></i> View project</a></li>
+                    <li><a href="{{ URL::to('/companies/') }}"><i class="fa fa-list"></i> All companies</a></li>
 
+                    @if (Auth::user()->role_id == 1)
+                        <br />
+                        <a href="#"
+                           onclick="
+                                   var result = confirm('Are you sure you wish to delete this task?');
+                                   if(result) {
+                                   event.preventDefault();
+                                   $('#delete-task-{{ $task->id }}').submit();
+                                   }"><i class="fa fa-trash"></i> Delete task</a>
+
+                        <form id="delete-task-{{ $task->id }}" action="{{ route("tasks.destroy", [$task->id]) }}"
+                              method="post" style="display: none">
+                            {{ csrf_field() }}
+                            <input type="hidden" name="_method" value="delete" />
+                        </form>
+                    @endif
+
+                </ol>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                @include("partials.comments")
+            </div>
         </div>
 
     </div>
